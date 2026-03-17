219183d032aee8bb98ed196af7e308c94de2e879
dianarudolph05-coder <diana.rudolph05@gmail.com>
2026-03-17 11:01:52 -0400

    Add files via upload

--- assignment-1/sketch.js
+++ assignment-1/sketch.js
@@ -1,17 +1,40 @@
 function setup() {
-  // Create a canvas as big as the container window
-  createCanvas(windowWidth, windowHeight);
-  textAlign(CENTER, CENTER);
-}
+  noCanvas();
 
-function draw() {
-  background(220);
-  textSize(height / 16);
-  text("Assignment[1]", width / 2, height / 2);
-}
-
-// This function is called any time the window is resized
-function windowResized() {
-  // Change the canvas size to fit the window
-  resizeCanvas(windowWidth, windowHeight);
+  // Name
+  let name = prompt("Hello, what is your name?");
+  alert("Welcome " + name + ", it's a pleasure to greet you!");
+  //hacker attempt
+  let old = prompt("May I ask how old are you?");
+  let birthyear = 2026 - old;
+  alert(
+    "In that case, you must have been born around " + birthyear + ", right? 😊"
+  );
+  // Two integers
+  let a = Number(prompt("Please enter an integer value:"));
+  let b = Number(prompt("Please enter a second integer value:"));
+  alert(
+    "Let me show you what I can do with the numbers " + a + " and " + b + ":"
+  );
+  //simple alerts
+  alert(a + " + " + b + " = " + (a + b));
+  alert(a + " - " + b + " = " + (a - b));
+  alert(a + " * " + b + " = " + a * b);
+  alert(a + " / " + b + " = " + a / b);
+  alert(a + " % " + b + " = " + (a % b));
+  alert("The max of " + a + " and " + b + " is " + max(a, b));
+  alert("The min of " + a + " and " + b + " is " + min(a, b));
+  // Decimal number
+  let x = Number(prompt("Please enter a value with a decimal part:"));
+  // fancy alerts
+  alert("Let me show you what I can do with the number " + x + ":");
+  alert("The negative of " + x + " is " + -x);
+  alert("The sine of " + x + " radians is " + sin(x));
+  alert("The cosine of " + x + " radians is " + cos(x));
+  alert(x + "^10 = " + pow(x, 10));
+  alert("Square root of " + x + " = " + sqrt(x));
+  alert("Rounded " + x + " = " + round(x));
+  alert("Floor of " + x + " = " + floor(x));
+  alert("Ceiling of " + x + " = " + ceil(x));
+  alert("Absolute value of " + x + " = " + abs(x));
 }
