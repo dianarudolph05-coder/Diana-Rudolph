c7389c6627316c319c6347371460ac849b8c8279
dianarudolph05-coder <diana.rudolph05@gmail.com>
2026-03-17 12:29:02 -0400

    Add files via upload

--- assignment-4/sketch.js
+++ assignment-4/sketch.js
@@ -1,17 +1,29 @@
+/*Class: ARTG-2262+63: Prototyping with Code
+Name: Diana Rudolph
+Email: rudolph.d@northeaswtern.edu
+Assignment: 6
+Instructions:
+- Click and drag to draw
+- Press R for red, G for green, B for blue, K for black
+- Use scroll wheel to change brush size
+- Press S to save*/
+let col;
+let brushSize = 10;
 function setup() {
-  // Create a canvas as big as the container window
-  createCanvas(windowWidth, windowHeight);
-  textAlign(CENTER, CENTER);
-}
-
+  createCanvas(800, 600);
+  background(255);
+  col = color(0);}
 function draw() {
-  background(220);
-  textSize(height / 16);
-  text("Assignment[4]", width / 2, height / 2);
-}
-
-// This function is called any time the window is resized
-function windowResized() {
-  // Change the canvas size to fit the window
-  resizeCanvas(windowWidth, windowHeight);
-}
+  if (mouseIsPressed) {
+    fill(col);
+    noStroke();
+    ellipse(mouseX, mouseY, brushSize, brushSize);}}
+function keyPressed() {
+  if (key === 'R' || key === 'r') col = color(255, 0, 0);
+  if (key === 'G' || key === 'g') col = color(0, 200, 0);
+  if (key === 'B' || key === 'b') col = color(0, 0, 255);
+  if (key === 'K' || key === 'k') col = color(0);
+  if (key === 'S' || key === 's') saveCanvas('my_drawing', 'png');}
+function mouseWheel(event) {
+  brushSize = constrain(brushSize - event.delta * 0.05, 2, 100);
+  return false;}
